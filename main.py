23b304e1c7595067c8ed738ae37e3d3de7caca5a
Daniel G Perico S <d.perico@dxc.com>
2021-07-24 08:27:20 -0500

    Version 2 de la API

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from typing import Optional
 from fastapi import FastAPI
 import pandas as pd
+from model import predict_starts
 
 df = pd.read_csv("reviews_file.csv")
+final_df = df.sort_values(by = "repliedAt")
 final_df = df.loc[:,["content","ModelStarts"]]
+
 
 app = FastAPI()
 
@@ -13,11 +16,11 @@ def read_root():
     return {"Hello": "Easy Data"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
 @app.get("/comments/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_pandas_df(item_id: int, q: Optional[str] = None):
     return final_df.loc[0:item_id,:].to_dict()
+
+
+@app.get("/predict/")
+def predict_string(q: Optional[str] = None):
+    return predict_starts(q)
--- model.py
+++ model.py
@@ -5,29 +5,48 @@ import pandas as pd
 from google_play_scraper import app, Sort, reviews
 from transformers import pipeline
 
-rvws, token = reviews(
-        'com.clarocolombia.miclaro', # app's ID, found in app's url
-        lang='es',            # defaults to 'en''
-        sort=Sort.NEWEST,     # defaults to Sort.MOST_RELEVANT
-        filter_score_with = None,  # defaults to None (get all scores)
-        count = 100             # defaults to 100
-        # , continuation_token=token
-    )
-
-reviews_pdf = pd.DataFrame(rvws)
 classifier = pipeline('sentiment-analysis', 
                       model="nlptown/bert-base-multilingual-uncased-sentiment")
 
-model_starts_list = []
-model_score_list = []
-results = classifier(list(reviews_pdf["content"]))
-for result in results:
-    model_starts_list.append(result['label'])
-    model_score_list.append(round(result['score'], 4))
+def predict_starts(string):
+    model_dict = classifier(string)
+    return type(model_dict)
+    print(model_dict)
+    if model_dict['label'] == '1':
+        model_dict['Sentimiento'] = "Muy Negativo"
+    elif model_dict['label'] == '2':
+        model_dict['Sentimiento'] = "Negativo"
+    elif model_dict['label'] == '1':
+        model_dict['Sentimiento'] = "Neutro"
+    elif model_dict['label'] == '1':
+        model_dict['Sentimiento'] = "Positivo"
+    else:
+        model_dict['Sentimiento'] = "Muy Positivo"
+    return model_dict
+    #return classifier(string)
+
+if __name__ == '__main__':
+    rvws, token = reviews(
+            'com.clarocolombia.miclaro', # app's ID, found in app's url
+            lang='es',            # defaults to 'en''
+            sort=Sort.NEWEST,     # defaults to Sort.MOST_RELEVANT
+            filter_score_with = None,  # defaults to None (get all scores)
+            count = 100             # defaults to 100
+            # , continuation_token=token
+        )
+
+    reviews_pdf = pd.DataFrame(rvws)
+    model_starts_list = []
+    model_score_list = []
+    results = classifier(list(reviews_pdf["content"]))
+    for result in results:
+        model_starts_list.append(result['label'])
+        model_score_list.append(round(result['score'], 4))
 
 
-reviews_pdf["ModelStarts"] = model_starts_list
-reviews_pdf["ModelStarts"] = reviews_pdf["ModelStarts"].apply(lambda x : int(x[0:1]))
-reviews_pdf["ModelScore"] = model_score_list
-reviews_pdf["StartsDifference"] = abs(reviews_pdf["score"] - reviews_pdf["ModelStarts"])
-reviews_pdf.to_csv('reviews_file.csv')
+    reviews_pdf["ModelStarts"] = model_starts_list
+    reviews_pdf["ModelStarts"] = reviews_pdf["ModelStarts"].apply(lambda x : int(x[0:1]))
+    reviews_pdf["ModelScore"] = model_score_list
+    reviews_pdf["StartsDifference"] = abs(reviews_pdf["score"] - reviews_pdf["ModelStarts"])
+    reviews_pdf.to_csv('reviews_file.csv')
+
